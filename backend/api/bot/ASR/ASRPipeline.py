bec0689a115bddda9012dc6f0848562d85443715
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2025-02-21 12:22:24 +0330

    feat: finalize ASR pipeline

--- backend/api/bot/ASR/ASRPipeline.py
+++ backend/api/bot/ASR/ASRPipeline.py
@@ -2,45 +2,42 @@ import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 # from datasets import load_dataset
 
+def feed_audio_to_ASR_modal(audio_path):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    model_id = "openai/whisper-large-v3-turbo"
 
-model_id = "openai/whisper-large-v3-turbo"
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-)
-model.to(device)
+    processor = AutoProcessor.from_pretrained(model_id)
 
-processor = AutoProcessor.from_pretrained(model_id)
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
 
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
-    device=device,
-)
+    # dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+    # sample = dataset[0]["audio"]
 
-# dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
-# sample = dataset[0]["audio"]
+    generate_kwargs = {
+        "max_new_tokens": 448,
+        "num_beams": 1,
+        "condition_on_prev_tokens": False,
+        "compression_ratio_threshold": 1.35,  # zlib compression ratio threshold (in token space)
+        "temperature": (0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+        "logprob_threshold": -1.0,
+        "no_speech_threshold": 0.6,
+        "return_timestamps": True,
+    }
 
-result = pipe("audio.mp3")
-
-print(result["text"])
-
-generate_kwargs = {
-    "max_new_tokens": 448,
-    "num_beams": 1,
-    "condition_on_prev_tokens": False,
-    "compression_ratio_threshold": 1.35,  # zlib compression ratio threshold (in token space)
-    "temperature": (0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
-    "logprob_threshold": -1.0,
-    "no_speech_threshold": 0.6,
-    "return_timestamps": True,
-}
-
-result = pipe("audio.mp3", generate_kwargs=generate_kwargs)
+    result = pipe(audio_path, generate_kwargs=generate_kwargs)
+    return result["text"]
 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, MessageView, end_session
+from .views import RegisterView, LoginView, MessageView, end_session, get_audio_message
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('message/', MessageView.as_view(), name='message'),
     path('end-session/', end_session, name='end-session'),
+    path('send-audio/', get_audio_message, name='send-audio'),
 ]
