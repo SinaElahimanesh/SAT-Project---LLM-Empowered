38f0fed837267d2b079ebd3cb69c94591ce57a14
Sina Elahimanesh <sinaaelahimanesh@gmail.com>
2025-11-27 23:07:10 +0100

    fix email saving

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,8 +4,13 @@ from .models import User, Message, Stage, UserGroup
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'stage', 'group')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'password', 'email', 'stage', 'group')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            # Do not force email required to avoid breaking existing clients;
+            # if provided, it will be saved.
+            'email': {'required': False, 'allow_blank': True}
+        }
 
     def create(self, validated_data):
         # Use the group from validated_data if present, otherwise default
@@ -13,6 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
+            email=validated_data.get('email', ''),
             stage=validated_data.get('stage', Stage.BEGINNING),
             group=group
         )
